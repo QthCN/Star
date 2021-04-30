2ff084a725f50a4d474cef3cad63e851f5603851
tianhuan <tianhuan@bingotree.cn>
2021-04-30 20:41:01 +0800

    always analysis java

--- Sources/Java/JavaAnalyzer.swift
+++ Sources/Java/JavaAnalyzer.swift
@@ -35,35 +35,8 @@ public class JavaAnalyzer: Analyzer {
     }
     
     func isJavaProject() -> Bool {
-        // 搜索三层目录看是否有java结尾的文件，有的话就认为是java的项目
-        let topDirItems = self.fs.listItems(path: FileSystemObject())
-        for topDirItem in topDirItems {
-            if !topDirItem.dir() && topDirItem.objName().hasSuffix(".java") {
-                return true
-            }
-            
-            if topDirItem.dir() {
-                let subDirItems = self.fs.listItems(path: topDirItem)
-                for subDirItem in subDirItems {
-                    if !subDirItem.dir() && subDirItem.objName().hasSuffix(".java") {
-                        return true
-                    }
-                }
-                
-                for subDirItem in subDirItems {
-                    if subDirItem.dir() {
-                        let subSubDirItems = self.fs.listItems(path: subDirItem)
-                        for subSubDirItem in subSubDirItems {
-                            if !subSubDirItem.dir() && subSubDirItem.objName().hasSuffix(".java") {
-                                return true
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        
-        return false
+        // TODO
+        return true
     }
     
     func getClassPaths() -> [String] {
